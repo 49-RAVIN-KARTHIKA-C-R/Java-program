92e7a8c2d72b8b6b540d687bbc9de99d3b7370e8
ravinkarthikacr21 <ravinkarthikacr1998@gmail.com>
2021-05-27 22:18:55 +0530

    9-13

---
+++ first 15 pgms/Array12.java
@@ -0,0 +1,9 @@
+public class Array12 {
+    public static void main(String[] args) {
+        int a[]={2,6,7,12,14};
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+
+    }
+  }
---
+++ first 15 pgms/Array13.java
@@ -0,0 +1,9 @@
+public class Array13{
+    public static void main(String[] args) {
+        String a[]={"Red","Blue","Green"};
+        for (String i:a){
+            System.out.println(i);
+
+        }
+    }
+  }
